92b91e580f8b9440585dc85e6aeeca37feb19646
pxr <pxr@DESKTOP-BIP5GPD>
2024-02-18 01:06:02 +0300

    Add project files.

---
+++ FoodyTekmerDataAccessLayer/Repositories/GenericRepository.cs
@@ -0,0 +1,42 @@
+﻿using FoodyTekmerDataAccessLayer.Abstract;
+using FoodyTekmerDataAccessLayer.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodyTekmerDataAccessLayer.Repositories
+{
+    public class GenericRepository<T> : IGenericDal<T> where T : class
+    {
+        FoodyContext context = new FoodyContext();
+        public void Delete(T t)
+        {
+            context.Remove(t);
+            context.SaveChanges();
+        }
+
+        public T GetById(int id)
+        {
+            return context.Set<T>().Find(id);
+        }
+
+        public List<T> GetList()
+        {
+            return context.Set<T>().ToList();
+        }
+
+        public void Insert(T t)
+        {
+            context.Add(t);
+            context.SaveChanges();
+        }
+
+        public void Update(T t)
+        {
+            context.Update(t);
+            context.SaveChanges();
+        }
+    }
+}
---
+++ FoodyTekmerWebUI/Controllers/AdminLayoutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FoodyTekmerWebUI.Controllers
+{
+    public class AdminLayoutController : Controller
+    {
+        public IActionResult _AdminLayout()
+        {
+            return View();
+        }
+    }
+}
